6988916ab25c919c1a95de6b4edd490c98e11610
gerardoalar <gerardo.alarcon2000@gmail.com>
2025-02-01 17:09:19 -0800

    Completed Assignment

--- main.cpp
+++ main.cpp
@@ -11,12 +11,17 @@ private:
 
 public:
     // Constructor
-
+    Robot(string n, string m, int b): name(n), model(m), batteryLife(b){}
 
     // Getter Methods
-
+    string getName(){return name;}
+    string getModel(){return model;}
+    int getBatteryLife(){return batteryLife;}
 
     // Setter Methods
+    void setName(string n){name = n;}
+    void setModel(string m){model = m;}
+    void setBatteryLife(int b){batteryLife = b;}
 
 
     // Display function
@@ -26,10 +31,14 @@ public:
 };
 
 // Step 2: Function to modify robot (pass by value)
-
+void modifyRobotByValue(Robot r) {
+    r.setBatteryLife(70);
+}
 
 // Step 3: Function to modify robot (pass by reference)
-
+void modifyRobotByReference(Robot& r) {
+    r.setBatteryLife(100);
+}
 
 // Step 4: Template class for a Fleet that stores multiple robots
 template <typename T>
@@ -73,20 +82,26 @@ public:
 
 int main() {
     // Step 5: Create a Robot object
-
-
+Robot robot1("Optimus","Transformer",50);
 
     // Step 6: Use pointers to access Robot object
+Robot* ptr = &robot1;
+cout<<"Robot object: "<<ptr<<endl;
 
    // cout << "Updated Battery Life (using pointer): " << robotPtr->getBatteryLife() << "%\n";
+cout<<"Updated Battery Life (using pointer): "<<ptr->getBatteryLife()<<"%\n";
 
     // Step 7: Pass by value (no change outside function)
+modifyRobotByValue(robot1);
 
    // cout << "After modifyRobotByValue, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+cout<<"After modifyRobotByValue, Battery Life: "<<robot1.getBatteryLife()<<"%\n";
 
     // Step 8: Pass by reference (changes persist)
+modifyRobotByReference(robot1);
 
    // cout << "After modifyRobotByReference, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+cout<<"After modifyRobotByReference, Battery Life: "<<robot1.getBatteryLife()<<"%\n";
 
     // Step 9: Use the Fleet template class
     Fleet<string> myFleet(3);
@@ -96,5 +111,10 @@ int main() {
 
     myFleet.showFleet();
 
+    Fleet<string> myFleet2(3);
+    myFleet2.addRobot("Twin Cities");
+    myFleet2.addRobot("Atom");
+    myFleet2.addRobot("Zeus");
+    myFleet2.showFleet();
     return 0;
 }
